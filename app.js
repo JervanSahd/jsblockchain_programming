010f7f0bd6af71a3cc8831e630e3540e56662b59
S.Snavely <info@stevesnavely.com>
2022-06-21 20:43:52 -0400

    completed proof of work

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const Block = require("./block");
 const Blockchain = require("./blockchain");
-const Transaction = require('./transaction')
+const Transaction = require("./transaction");
 
-let transaction = new Transaction ('Mary', 'John', 100)
+let transaction = new Transaction("Mary", "John", 100);
 
 // create the genesis block
 let genesisBlock = new Block();
 // create a blockchain with genesisBlock
 let blockchain = new Blockchain(genesisBlock);
+
+let block = blockchain.getNextBlock([transaction]);
+blockchain.addBlock(block);
+
+
+let anotherTransaction = new Transaction ('Steve', 'Brian', 500)
+let block1 = blockchain.getNextBlock([anotherTransaction])
+blockchain.addBlock(block1)
 
 console.log(blockchain);
--- blockchain.js
+++ blockchain.js
@@ -1,6 +1,8 @@
 // allows to create the hash
 const sha256 = require("js-sha256");
 
+const Block = require("./block");
+
 class Blockchain {
   constructor(genesisBlock) {
     this.blocks = [];
@@ -18,9 +20,35 @@ class Blockchain {
     // pushes block to blockchain
     this.blocks.push(block);
   }
+  //   sends array of transaction
+  getNextBlock(transactions) {
+    let block = new Block();
+    transactions.forEach((transaction) => {
+      // adds transaction to the block
+      block.addTransaction(transaction);
+    });
+    // get access to previous hash
+    let previousBlock = this.getPreviousBlock();
+    block.index = this.blocks.length;
+    block.previousHash = previousBlock.hash;
+    block.hash = this.generateHash(block);
+    return block;
+  }
+  //   returns previous block
+  getPreviousBlock() {
+    return this.blocks[this.blocks.length - 1];
+  }
+
   generateHash(block) {
     // generates new hash with help of npm package js-sha256
-    const hash = sha256(block.key);
+    let hash = sha256(block.key);
+    // will create a hash with 3 or 4 zeros
+    // if not the nonce
+    while (!hash.startsWith("0000")) {
+      block.nonce += 1;
+      hash = sha256(block.key);
+      console.log(hash);
+    }
     return hash;
   }
 }
