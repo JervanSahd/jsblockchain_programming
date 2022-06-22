2c4edc3451d5b646faf550a9bfa7ab8b524af52f
S.Snavely <info@stevesnavely.com>
2022-06-22 07:45:07 -0400

    Setup express

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const Block = require("./block");
 const Blockchain = require("./blockchain");
 const Transaction = require("./transaction");
 
-let transaction = new Transaction("Mary", "John", 100);
+const express = require("express");
+const app = express();
 
-// create the genesis block
-let genesisBlock = new Block();
-// create a blockchain with genesisBlock
-let blockchain = new Blockchain(genesisBlock);
+app.get("/blockchain", (req, res) => {
+  let transaction = new Transaction("Mary", "John", 100);
 
-let block = blockchain.getNextBlock([transaction]);
-blockchain.addBlock(block);
+  // create the genesis block
+  let genesisBlock = new Block();
+  // create a blockchain with genesisBlock
+  let blockchain = new Blockchain(genesisBlock);
 
+  let block = blockchain.getNextBlock([transaction]);
+  blockchain.addBlock(block);
 
-let anotherTransaction = new Transaction ('Steve', 'Brian', 500)
-let block1 = blockchain.getNextBlock([anotherTransaction])
-blockchain.addBlock(block1)
+  let anotherTransaction = new Transaction("Steve", "Brian", 500);
+  let block1 = blockchain.getNextBlock([anotherTransaction]);
+  blockchain.addBlock(block1);
 
-console.log(blockchain);
+  res.json(blockchain)
+});
+
+app.listen(8080, () => {
+  console.log("Server is running...");
+});
