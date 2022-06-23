1fda3306e401066d4b35f2108e5d3e71a5680efe
S.Snavely <info@stevesnavely.com>
2022-06-23 00:05:34 -0400

    updated resolve conflict

--- README.md
+++ README.md
@@ -22,11 +22,23 @@ Mining Blocks
 
 ```
 
+## DECENTRALIZING BLOCKCHAIN
+
+``` 
+Creating the node model
+Passing arguments to the Node app from command line
+Registering nodes
+Mining nodes
+Resolving conflicts
+
+```
+
 ## DEPENDENCIES
 ````
 "dependencies": {
     "express": "^4.18.1",
     "js-sha256": "^0.9.0"
+    "node-fetch": "^2.6.7"
   },
   "devDependencies": {
     "nodemon": "^2.0.16"
--- app.js
+++ app.js
@@ -1,18 +1,78 @@
 const Block = require("./block");
 const Blockchain = require("./blockchain");
 const Transaction = require("./transaction");
+const BlockchainNode = require("./blockchainNode");
+const fetch = require("node-fetch");
 
 const express = require("express");
 const app = express();
 
+const arguments = process.argv;
+let PORT = 8080;
+
+if (arguments.length > 2) {
+  PORT = arguments[2];
+}
+//body parser for JSON
+app.use(express.json());
+
 let transactions = [];
+// adds nodes to an array
+let nodes = [];
+let allTransactions = [];
 
 // creates variables for the blockchain
 let genesisBlock = new Block();
 let blockchain = new Blockchain(genesisBlock);
 
-//body parser for JSON
-app.use(express.json());
+app.get("/resolve", (req, res) => {
+  nodes.forEach((node) => {
+    fetch(`${node.url}/blockchain`)
+      .then((response) => response.json())
+      .then((otherBlockchain) => {
+        if (blockchain.blocks.length < otherBlockchain.blocks.length) {
+          allTransactions.forEach((transaction) => {
+            fetch(`${node.url}/transcations`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(transaction),
+            })
+              .then((response) => response.json())
+              .then((_) => {
+                fetch(`${node.url}/mine`)
+                  .then((response) => response.json())
+                  .then((_) => {
+                    fetch(`${node.url}/blockchain`)
+                      .then((response) => response.json())
+                      .then((updatedBlockchain) => {
+                        console.log(updatedBlockchain);
+                        blockchain = updatedBlockchain;
+                        res.json(blockchain);
+                      });
+                  });
+              });
+          });
+        } else {
+          res.json(blockchain);
+        }
+      });
+  });
+});
+
+app.post("/nodes/register", (req, res) => {
+  // requesting the urls of the nodes
+  const urls = req.body;
+  // go through the urls to get the url strings
+  urls.forEach((url) => {
+    //  create instance of blockchain node
+    const node = new BlockchainNode(url);
+    nodes.push(node);
+  });
+  // returns back all nodes that have been registered
+  res.json(nodes);
+});
 
 app.post("/transactions", (req, res) => {
   const to = req.body.to;
@@ -34,27 +94,17 @@ app.get("/mine", (req, res) => {
   // adds block to the blockchain
   blockchain.addBlock(block);
   // returns the block
+  transactions.forEach((transaction) => {
+    allTransactions.push(transaction);
+  });
+  transactions = [];
   res.json(block);
 });
 
 app.get("/blockchain", (req, res) => {
-  //   let transaction = new Transaction("Mary", "John", 100);
-
-  //   // create the genesis block
-  //   let genesisBlock = new Block();
-  //   // create a blockchain with genesisBlock
-  //   let blockchain = new Blockchain(genesisBlock);
-
-  //   let block = blockchain.getNextBlock([transaction]);
-  //   blockchain.addBlock(block);
-
-  //   let anotherTransaction = new Transaction("Steve", "Brian", 500);
-  //   let block1 = blockchain.getNextBlock([anotherTransaction]);
-  //   blockchain.addBlock(block1);
-
   res.json(blockchain);
 });
 
-app.listen(8080, () => {
-  console.log("Server is running...");
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
 });
